0fa4aca93c927a56c61317225ec1ea459f99702f
Mike Bergstrom <mikedberg@hotmail.com>
2017-05-26 15:35:51 -0700

    initial commit

---
+++ I Do Declare!.js
@@ -0,0 +1,22 @@
+var number1 = 3.14159;
+var number2 = 100.1;
+var number3 = -34;
+var number4 = .90876;
+var string1 = "This is the first string!"
+var string2 = "Coding Dojo";
+var string3 = "Hello World!"
+var skyisblue = true;
+var upisdown = false;
+var undefined;
+
+console.log("The first number:", number1);
+console.log("The second number:", number2);
+console.log("The third number:", number3);
+console.log("The fourth number:", number4);
+console.log("First string:", string1);
+console.log("The second string:", string2);
+console.log("The third string:", string3);
+console.log("The sky is blue?", boolean1);
+console.log("Up is down?", boolean2);
+console.log("This variable is undefined:" undefined);
+
